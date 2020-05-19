576113b0795859cb09a1d6e624cefacf2da0002d
James Corbould <james.bron@hotmail.com>
2020-05-19 21:04:43 +1200

    Initial commit.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Header } from './Header';
+import { HomePage } from './HomePage';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import { fontFamily, fontSize, gray2 } from './Styles';
+
+function App() {
+  return (
+    <div
+      css={css`
+        font-family: ${fontFamily};
+        font-size: ${fontSize};
+        color: ${gray2};
+      `}
+    >
+      <Header />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
