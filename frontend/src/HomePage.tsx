e0b8bffc563476348233f03c1edf289dfc5577cb
James Corbould <james.bron@hotmail.com>
2020-05-25 20:59:43 +1200

    Added generate page and pagetitle components.

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -4,35 +4,29 @@ import { PrimaryButton } from './Styles';
 import { QuestionList } from './QuestionList';
 import { getUnansweredQuestions } from './QuestionsData';
 import { Question } from './Question';
+import { Page } from './Page';
+import { PageTitle } from './PageTitle';
 
 export const HomePage = () => (
-  <div
-    css={css`
-      margin: 50px auto 20px auto;
-      padding: 30px 20px;
-      max-width: 600px;
-    `}
-  >
+  <Page>
     <div
       css={css`
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
+        margin: 50px auto 20px auto;
+        padding: 30px 20px;
+        max-width: 600px;
       `}
     >
-      <h2
+      <div
         css={css`
-          font-size: 15px;
-          font-weight: bold;
-          margin: 10px 0px 5px;
-          text-align: center;
-          text-transform: uppercase;
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
         `}
       >
-        Unanswered Questions
-      </h2>
-      <PrimaryButton>Ask a question</PrimaryButton>
+        <PageTitle>Unanswered Questions</PageTitle>
+        <PrimaryButton>Ask a question</PrimaryButton>
+      </div>
+      <QuestionList data={getUnansweredQuestions()} />
     </div>
-    <QuestionList data={getUnansweredQuestions()} />
-  </div>
+  </Page>
 );
--- frontend/src/Question.tsx
+++ frontend/src/Question.tsx
@@ -1,13 +1,14 @@
 /** @jsx jsx */ import { css, jsx } from '@emotion/core';
 import { FC } from 'react';
 import { QuestionData } from './QuestionsData';
-import { gray3 } from './Styles';
+import { gray2, gray3 } from './Styles';
 
 interface Props {
   data: QuestionData;
+  showContent?: boolean;
 }
 
-export const Question: FC<Props> = ({ data }) => (
+export const Question: FC<Props> = ({ data, showContent = true }) => (
   <div
     css={css`
       padding: 10px 0px;
@@ -21,6 +22,19 @@ export const Question: FC<Props> = ({ data }) => (
     >
       {data.title}
     </div>
+    {showContent && (
+      <div
+        css={css`
+          padding-bottom: 10px;
+          font-size: 15px;
+          color: ${gray2};
+        `}
+      >
+        {data.content.length > 50
+          ? `${data.content.substring(0, 50)}...`
+          : data.content}
+      </div>
+    )}
     <div
       css={css`
         font-size: 12px;
