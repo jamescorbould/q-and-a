b038e86923f24c6f61a3510e301fbb6ce15e13d1
James Corbould <james.bron@hotmail.com>
2020-05-27 21:37:46 +1200

    Added state components.

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -2,31 +2,56 @@
 import { css, jsx } from '@emotion/core';
 import { PrimaryButton } from './Styles';
 import { QuestionList } from './QuestionList';
-import { getUnansweredQuestions } from './QuestionsData';
+import { getUnansweredQuestions, QuestionData } from './QuestionsData';
 import { Question } from './Question';
 import { Page } from './Page';
 import { PageTitle } from './PageTitle';
+import { useEffect, useState } from 'react';
 
-export const HomePage = () => (
-  <Page>
-    <div
-      css={css`
-        margin: 50px auto 20px auto;
-        padding: 30px 20px;
-        max-width: 600px;
-      `}
-    >
+export const HomePage = () => {
+  const [questions, setQuestions] = useState<QuestionData[] | null>(null);
+  const [questionsLoading, setQuestionsLoading] = useState(true);
+  useEffect(() => {
+    const doGetUnansweredQuestions = async () => {
+      const unansweredQuestions = await getUnansweredQuestions();
+      setQuestions(unansweredQuestions);
+      setQuestionsLoading(false);
+    };
+    doGetUnansweredQuestions();
+  }, []);
+  console.log('rendered');
+  return (
+    <Page>
       <div
         css={css`
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
+          margin: 50px auto 20px auto;
+          padding: 30px 20px;
+          max-width: 600px;
         `}
       >
-        <PageTitle>Unanswered Questions</PageTitle>
-        <PrimaryButton>Ask a question</PrimaryButton>
+        <div
+          css={css`
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+          `}
+        >
+          <PageTitle>Unanswered Questions</PageTitle>
+          <PrimaryButton>Ask a question</PrimaryButton>
+        </div>
+        {questionsLoading ? (
+          <div
+            css={css`
+              font-size: 16px;
+              font-style: italic;
+            `}
+          >
+            Loading...
+          </div>
+        ) : (
+          <QuestionList data={questions || []} />
+        )}
       </div>
-      {/* <QuestionList data={getUnansweredQuestions()} /> */}
-    </div>
-  </Page>
-);
+    </Page>
+  );
+};
