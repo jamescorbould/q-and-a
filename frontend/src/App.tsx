e0c5ab5b468ace71c3f9f5734f9c44180ae7cbc3
James Corbould <james.bron@hotmail.com>
2020-06-03 21:43:04 +1200

    Added routing.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,19 +4,31 @@ import { HomePage } from './HomePage';
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
 import { fontFamily, fontSize, gray2 } from './Styles';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import { AskPage } from './AskPage';
+import { SearchPage } from './SearchPage';
+import { SignInPage } from './SignInPage';
 
 function App() {
   return (
-    <div
-      css={css`
-        font-family: ${fontFamily};
-        font-size: ${fontSize};
-        color: ${gray2};
-      `}
-    >
-      <Header />
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div
+        css={css`
+          font-family: ${fontFamily};
+          font-size: ${fontSize};
+          color: ${gray2};
+        `}
+      >
+        <Header />
+        <Switch>
+          <Redirect from="/home" to="/" />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/search" component={SearchPage} />
+          <Route path="/ask" component={AskPage} />
+          <Route path="/signin" component={SignInPage} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
