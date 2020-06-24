73938bc73d41d8c333fe6be8b21a18f94a1dbe6e
James Corbould <james.bron@hotmail.com>
2020-06-24 21:51:59 +1200

    Added generic form validators.

--- frontend/src/Form.tsx
+++ frontend/src/Form.tsx
@@ -6,27 +6,80 @@ export interface Values {
   [key: string]: any;
 }
 
+export interface Errors {
+  [key: string]: string[];
+}
+
+export interface Touched {
+  [key: string]: boolean;
+}
+
 interface FormContextProps {
   values: Values;
   setValue?: (fieldName: string, value: any) => void;
+  errors: Errors;
+  validate?: (fieldName: string) => void;
+  touched: Touched;
+  setTouched?: (fieldName: string) => void;
 }
 
 export const FormContext = createContext<FormContextProps>({
   values: {},
+  errors: {},
+  touched: {},
 });
+
+type Validator = (value: any, args?: any) => string;
+
+export const required: Validator = (value: any): string =>
+  value === undefined || value === null || value === ''
+    ? 'This must be populated'
+    : '';
+
+export const minLength: Validator = (value: any, length: number): string =>
+  value && value.length < length
+    ? `This must be at least ${length} characters`
+    : '';
+
+interface Validation {
+  validator: Validator;
+  arg?: any;
+}
+
+interface ValidationProp {
+  [key: string]: Validation | Validation[];
+}
 
 interface Props {
   submitCaption?: string;
+  validationRules?: ValidationProp;
 }
 
-export const Form: FC<Props> = ({ submitCaption, children }) => {
+export const Form: FC<Props> = ({
+  submitCaption,
+  children,
+  validationRules,
+}) => {
   const [values, setValues] = useState<Values>({});
+  const [errors, setErrors] = useState<Errors>({});
+  const [touched, setTouched] = useState<Touched>({});
+
+  const validate = (fieldName: string): string[] => {
+    return [];
+  };
+
   return (
     <FormContext.Provider
       value={{
         values,
         setValue: (fieldName: string, value: any) => {
           setValues({ ...values, [fieldName]: value });
+        },
+        errors,
+        validate,
+        touched,
+        setTouched: (fieldName: string) => {
+          setTouched({ ...touched, [fieldName]: true });
         },
       }}
     >
