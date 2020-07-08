f1506a7144a67ec87367cccf8d3dd61056951a75
James Corbould <james.bron@hotmail.com>
2020-07-08 22:13:57 +1200

    Added form submission handlers.

--- frontend/src/QuestionPage.tsx
+++ frontend/src/QuestionPage.tsx
@@ -3,9 +3,9 @@ import { gray3, gray6 } from './Styles';
 import { FC, useState, Fragment, useEffect } from 'react';
 import { Page } from './Page';
 import { RouteComponentProps } from 'react-router-dom';
-import { QuestionData, getQuestion } from './QuestionsData';
+import { QuestionData, getQuestion, postAnswer } from './QuestionsData';
 import { AnswerList } from './AnswerList';
-import { Form, required, minLength } from './Form';
+import { Form, required, minLength, Values } from './Form';
 import { Field } from './Field';
 
 interface RouteParams {
@@ -27,6 +27,17 @@ export const QuestionPage: FC<RouteComponentProps<RouteParams>> = ({
       doGetQuestion(questionId);
     }
   }, [match.params.questionId]);
+
+  const handleSubmit = async (values: Values) => {
+    const result = await postAnswer({
+      questionId: question!.questionId,
+      content: values.content,
+      userName: 'Fred',
+      created: new Date(),
+    });
+
+    return { success: result ? true : false };
+  };
 
   return (
     <Page>
@@ -83,6 +94,9 @@ export const QuestionPage: FC<RouteComponentProps<RouteParams>> = ({
                     { validator: minLength, arg: 50 },
                   ],
                 }}
+                onSubmit={handleSubmit}
+                failureMessage="There was a problem with your answer"
+                successMessage="Your answer was successfully submitted"
               >
                 <Field name="content" label="Your Answer" type="TextArea" />
               </Form>
