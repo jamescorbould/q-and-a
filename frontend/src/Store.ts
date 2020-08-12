c0b7de7e93504302b09b6165c0010a7e6b22f2a4
James Corbould <james.bron@hotmail.com>
2020-08-12 21:52:59 +1200

    Initial commit for Redux changes.

---
+++ frontend/src/Store.ts
@@ -0,0 +1,99 @@
+import { Action, ActionCreator, Dispatch } from 'redux';
+import {
+  QuestionData,
+  getUnansweredQuestions,
+  postQuestion,
+  PostQuestionData,
+} from './QuestionsData';
+import { ThunkAction } from 'redux-thunk';
+
+interface QuestionsState {
+  readonly loading: boolean;
+  readonly unanswered: QuestionData[] | null;
+  readonly postedResult?: QuestionData;
+}
+
+interface GettingUnansweredQuestionsAction
+  extends Action<'GettingUnansweredQuestions'> {}
+
+export interface GotUnansweredQuestionsAction
+  extends Action<'GotUnansweredQuestions'> {
+  questions: QuestionData[];
+}
+
+export interface PostedQuestionAction extends Action<'PostedQuestion'> {
+  result: QuestionData | undefined;
+}
+
+export interface AppState {
+  readonly questions: QuestionsState;
+}
+
+type QuestionsActions =
+  | GettingUnansweredQuestionsAction
+  | GotUnansweredQuestionsAction
+  | PostedQuestionAction;
+
+const initialQuestionState: QuestionsState = {
+  loading: false,
+  unanswered: null,
+};
+
+/* export const getUnansweredQuestionsActionCreator = () => {
+  return async (dispatch: Dispatch) => {
+    const gettingUnansweredQuestionsAction: GettingUnansweredQuestionsAction = {
+      type: 'GettingUnansweredQuestions',
+    };
+    dispatch(gettingUnansweredQuestionsAction);
+    const questions = await getUnansweredQuestions();
+    const GotUnansweredQuestions: GotUnansweredQuestionsAction = {
+      questions,
+      type: 'GotUnansweredQuestions',
+    };
+    dispatch(gotUnansweredQuestionAction);
+  };
+}; */
+
+export const getUnansweredQuestionsActionCreator: ActionCreator<ThunkAction<
+  Promise<void>,
+  QuestionData[],
+  null,
+  GotUnansweredQuestionsAction
+>> = () => {
+  return async (dispatch: Dispatch) => {
+    const gettingUnansweredQuestionsAction: GettingUnansweredQuestionsAction = {
+      type: 'GettingUnansweredQuestions',
+    };
+    dispatch(gettingUnansweredQuestionsAction);
+    const questions = await getUnansweredQuestions();
+    const gotUnansweredQuestionAction: GotUnansweredQuestionsAction = {
+      questions,
+      type: 'GotUnansweredQuestions',
+    };
+    dispatch(gotUnansweredQuestionAction);
+  };
+};
+
+export const postQuestionActionCreator: ActionCreator<ThunkAction<
+  Promise<void>,
+  QuestionData,
+  PostQuestionData,
+  PostedQuestionAction
+>> = (question: PostQuestionData) => {
+  return async (dispatch: Dispatch) => {
+    const result = await postQuestion(question);
+    const postedQuestionAction: PostedQuestionAction = {
+      type: 'PostedQuestion',
+      result,
+    };
+    dispatch(postedQuestionAction);
+  };
+};
+
+export const clearPostedQuestionActionCreator: ActionCreator<PostedQuestionAction> = () => {
+  const postedQuestionAction: PostedQuestionAction = {
+    type: 'PostedQuestion',
+    result: undefined,
+  };
+  return postedQuestionAction;
+};
