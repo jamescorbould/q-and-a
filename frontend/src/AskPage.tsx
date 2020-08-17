63d62586ce6a710784d9e200da385bb6b0c49d2b
James Corbould <james.bron@hotmail.com>
2020-08-17 21:53:59 +1200

    Managing state in the ask page using Redux.

--- frontend/src/AskPage.tsx
+++ frontend/src/AskPage.tsx
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, { FC, useEffect } from 'react';
 import { Page } from './Page';
-import { Form, required, minLength, between, Values } from './Form';
+import {
+  Form,
+  required,
+  minLength,
+  between,
+  Values,
+  SubmitResult,
+} from './Form';
 import { Field } from './Field';
-import { postQuestion } from './QuestionsData';
+import { PostQuestionData, QuestionData } from './QuestionsData';
+import { connect } from 'react-redux';
+import { ThunkDispatch } from 'redux-thunk';
+import {
+  postQuestionActionCreator,
+  AppState,
+  clearPostedQuestionActionCreator,
+} from './Store';
+import { AnyAction } from 'redux';
 
-export const AskPage = () => {
-  const handleSubmit = async (values: Values) => {
-    const question = await postQuestion({
+interface Props {
+  postQuestion: (question: PostQuestionData) => Promise<void>;
+  postedQuestionResult?: QuestionData;
+  clearPostedQuestion: () => void;
+}
+
+const AskPage: FC<Props> = ({
+  postQuestion,
+  postedQuestionResult,
+  clearPostedQuestion,
+}) => {
+  useEffect(() => {
+    return function cleanUp() {
+      clearPostedQuestion();
+    };
+  }, [clearPostedQuestion]);
+
+  const handleSubmit = (values: Values) => {
+    postQuestion({
       title: values.title,
       content: values.content,
       userName: 'Fred',
       created: new Date(),
     });
-    return { success: question ? true : false };
   };
+
+  let submitResult: SubmitResult | undefined;
+  if (postedQuestionResult) {
+    submitResult = { success: postedQuestionResult !== undefined };
+  }
+
   return (
     <Page title="Ask a question">
       <Form
@@ -27,6 +63,7 @@ export const AskPage = () => {
           ],
         }}
         onSubmit={handleSubmit}
+        submitResult={submitResult}
         failureMessage="There was a problem with your question"
         successMessage="Your question was successfully submitted"
       >
@@ -38,4 +75,18 @@ export const AskPage = () => {
   );
 };
 
-export default AskPage;
+const mapStateToProps = (store: AppState) => {
+  return {
+    postedQuestionResult: store.questions.postedResult,
+  };
+};
+
+const mapDispatchToProps = (dispatch: ThunkDispatch<any, any, AnyAction>) => {
+  return {
+    postQuestion: (question: PostQuestionData) =>
+      dispatch(postQuestionActionCreator(question)),
+    clearPostedQuestion: () => dispatch(clearPostedQuestionActionCreator()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AskPage);
--- frontend/src/Form.tsx
+++ frontend/src/Form.tsx
@@ -68,7 +68,8 @@ interface ValidationProp {
 interface Props {
   submitCaption?: string;
   validationRules?: ValidationProp;
-  onSubmit: (values: Values) => Promise<SubmitResult>;
+  onSubmit: (values: Values) => Promise<SubmitResult> | void;
+  submitResult?: SubmitResult;
   successMessage?: string;
   failureMessage?: string;
 }
@@ -78,6 +79,7 @@ export const Form: FC<Props> = ({
   children,
   validationRules,
   onSubmit,
+  submitResult,
   successMessage = 'Success!',
   failureMessage = 'Something went wrong',
 }) => {
@@ -116,6 +118,12 @@ export const Form: FC<Props> = ({
       setSubmitting(true);
       setSubmitError(false);
       const result = await onSubmit(values);
+
+      // The result may be passed through as a prop.
+      if (result === undefined) {
+        return;
+      }
+
       setErrors(result.errors || {});
       setSubmitError(!result.success);
       setSubmitting(false);
@@ -138,6 +146,16 @@ export const Form: FC<Props> = ({
     }
   };
 
+  const disabled = submitResult
+    ? submitResult.success
+    : submitting || (submitted && !submitError);
+  const showError = submitResult
+    ? !submitResult.success
+    : submitted && submitError;
+  const showSuccess = submitResult
+    ? submitResult.success
+    : submitted && !submitError;
+
   return (
     <FormContext.Provider
       value={{
@@ -155,7 +173,7 @@ export const Form: FC<Props> = ({
     >
       <form noValidate={true} onSubmit={handleSubmit}>
         <fieldset
-          disabled={submitting || (submitted && !submitError)}
+          disabled={disabled}
           css={css`
             margin: 10px auto 0 auto;
             padding: 30px;
@@ -176,7 +194,7 @@ export const Form: FC<Props> = ({
           >
             <PrimaryButton type="submit"> {submitCaption} </PrimaryButton>
           </div>
-          {submitted && submitError && (
+          {showError && (
             <p
               css={css`
                 color: red;
@@ -185,7 +203,7 @@ export const Form: FC<Props> = ({
               {failureMessage}
             </p>
           )}
-          {submitted && !submitError && (
+          {showSuccess && (
             <p
               css={css`
                 color: green;
