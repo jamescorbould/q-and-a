2fa8db891566f1b51d51e70b387137d55c71af5c
James Corbould <james.bron@hotmail.com>
2020-08-24 21:30:20 +1200

    Initial commit - added Dapper and repository class.

---
+++ backend/QandA/Data/DataRepository.cs
@@ -0,0 +1,62 @@
+﻿using System.Data.SqlClient;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using QandA.Data.Models;
+
+namespace QandA.Data
+{
+    class DataRepository : IDataRepository
+    {
+        private readonly string _connectionString;
+
+        public DataRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration["ConnectionString:DefaultConnection"];
+        }
+
+        public AnswerGetResponse GetAnswer(int answerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public QuestionGetSingleResponse GetQuestion(int questionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<QuestionGetManyResponse> GetQuestions()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                return connection.Query<QuestionGetManyResponse>(
+                    @"EXEC dbo.Question_GetMany"
+                );
+            }
+        }
+
+        public IEnumerable<QuestionGetManyResponse> GetQuestionsBySearch(string search)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                return connection.Query<QuestionGetManyResponse>(
+                    @"EXEC dbo.Question_GetMany_BySearch @Search = @Search",
+                    new { Search = search }
+                );
+            }
+        }
+
+        public IEnumerable<QuestionGetManyResponse> GetUnansweredQuestions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool QuestionExists(int questionId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ backend/QandA/Data/Models/QuestionGetManyResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace QandA.Data.Models
+{
+    class QuestionGetManyResponse
+    {
+        public int QuestionId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string UserName { get; set; }
+        public DateTime Created
+        {
+            get; set;
+        }
+    }
+}
